c9a3c6e55115b83189bdda1ae0a857369521a952
Zamitou <141634733+Zamitou@users.noreply.github.com>
2023-12-30 15:00:02 +0000

    Delete config/scripts/addchromiumdesktopfile.sh

--- config/scripts/addchromiumdesktopfile.sh
+++
@@ -1,7 +0,0 @@
-
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-sed 's/launcher_thunar = thunar.desktop/launcher_thunar = thunar.desktop\nlauncher_chromium = chromium-browser.desktop/' /usr/share/wayfire/wf-shell.ini 
